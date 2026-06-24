01727dbab8a92a5298998931992d29f82ca21372
John <john.usa.james@gmail.com>
2026-06-24 20:04:10 +0100

    chore: config type aware lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,34 @@
 import js from "@eslint/js"
 import globals from "globals"
 import tseslint from "typescript-eslint"
-import { defineConfig } from "eslint/config"
+import { defineConfig, globalIgnores } from "eslint/config"
 
 export default defineConfig([
+  globalIgnores(["**/dist"]),
   {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.node },
+    files: ["**/*.{js,mjs,cjs}"],
+    extends: [js.configs.recommended],
+    languageOptions: {
+      globals: globals.node,
+    },
   },
-  tseslint.configs.recommended,
+  {
+    files: ["**/*.{ts,mts,cts}"],
+    extends: [tseslint.configs.recommended, tseslint.configs.recommendedTypeChecked],
+    languageOptions: {
+      globals: globals.node,
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
 ])
